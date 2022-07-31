2659f702c1a5f7b2f51f4198d4063e468ba7b9ef
William Ferreira Dantas Neto <65333545+ryuyayokito@users.noreply.github.com>
2022-07-31 17:27:05 -0300

    Update and rename print.py to Aula1.py

---
+++ Aula1.py
@@ -0,0 +1,7 @@
+print('Hello World!')
+print('olá', 'ryuya', sep='-')  # escolhendo - como separação
+# alterando o separador padrão de pular linha
+print('olá', 'ryuya', end=' <> ')
+print('yookito')
+print('000', '000', '000', sep='.', end='-')
+print('00')
