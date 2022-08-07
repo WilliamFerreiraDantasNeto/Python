b30e53f552e6d6e0239a9cf81a404df18884ea9e
William Ferreira Dantas Neto <65333545+ryuyayokito@users.noreply.github.com>
2022-08-07 20:26:56 -0300

    Create 02string.py

---
+++ 02string.py
@@ -0,0 +1,10 @@
+"""
+str - string
+"""
+
+print('Com aspa simples')
+print("Com aspa dupla")
+print("string com uma aspa dentro 'algo' dentro")
+print('string com uma aspa dentro "algo" dentro')
+# r antes da string para ignorar comandos como o /n (pular linha)
+print(r"\n serve pra pular")
