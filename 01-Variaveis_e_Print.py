a56e9ca5cb55c67c61e432352bea560abc55353b
William Ferreira Dantas Neto <65333545+ryuyayokito@users.noreply.github.com>
2023-02-02 10:38:08 -0300

    Add files via upload

---
+++ 01-Variaveis_e_Print.py
@@ -0,0 +1,13 @@
+nome = 'William'
+sobrenome = 'Ferreira Dantas Neto'
+ano_nascimento = 1987
+idade = 2023 - ano_nascimento
+maior_de_idade = idade >= 18
+altura_metros = 1.70
+
+print('Nome:', nome)
+print('sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('altura em metros:', altura_metros)
---
+++ 02-ICM.py
@@ -0,0 +1,7 @@
+nome = 'William Ferreira'
+altura = 1.70
+peso = 102.5
+imc = peso/altura**2
+print(nome,'tem', altura, 'm de altura,')
+print('pesa', peso, 'kg e seu IMC é')
+print(imc)
