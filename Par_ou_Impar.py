7907e69bd5f0fb1f3ac722ae7780ab01dd8c414d
William Ferreira Dantas Neto <master17ce@hotmail.com>
2023-02-08 10:09:46 -0300

    alterando if para try

--- Par_ou_Impar.py
+++ Par_ou_Impar.py
@@ -3,16 +3,17 @@ Faça um programa que peça ao usuário para digitar um número inteiro,
 informe se este número é par ou ímpar. 
 Caso o usuário não digite um número inteiro,
 informe que não é um número inteiro.
+
 """
 
 num = input('Digite um numero inteiro: ')
 
-if num.isdigit():
-    num=int(num)
+try:
+    num = int(num)
     if num % 2 == 0:
         print(f'{num} é par')
     else:
         print(f'{num} é impar')
-else:
-    print('Não digitou um numero inteiro')
+except:
+    print('não digitou um numero inteiro')
     
