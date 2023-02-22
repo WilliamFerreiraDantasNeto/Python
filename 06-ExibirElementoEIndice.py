ac6872bd223be68b32178141703a478d98dbed0a
William Ferreira Dantas Neto <master17ce@hotmail.com>
2023-02-22 11:51:52 -0300

    Desafio imprimir indices lista e elemento

---
+++ 06-ExibirElementoEIndice.py
@@ -0,0 +1,10 @@
+"""
+Exiba os índices da lista
+0 Maria
+1 Helena
+2 Luiz
+"""
+lista = ['Maria', 'Helena', 'Luiz']
+indices = range(len(lista))
+for indice in indices:
+    print(indice, lista[indice])
