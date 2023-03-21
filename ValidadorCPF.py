d894fe2fcd3febc280075c0de1c970501aea8029
William Ferreira Dantas Neto <master17ce@hotmail.com>
2023-03-21 17:54:16 -0300

    Melhoramento de codigo

--- ValidadorCPF.py
+++ ValidadorCPF.py
@@ -1,27 +1,25 @@
-CPF = input('Digite o seu CPF com pontos e traço: ')
-CPF2 = ''
-CPFteste = ''
-for x in CPF:
-    if not x == '.' and not x == '-':
-        CPFteste = CPFteste+x
+CPF = input('Digite o seu CPF com pontos e traço: ')\
+    .replace ('.', '')\
+    .replace ('-','')\
+    .replace (' ','')
+
+CPF2 = CPF[:9]
 cont = 10
 soma = 0
-for x in CPF:
-    if x == '-':
-        break
-    elif not x == '.':
-        CPF2 = CPF2+x
-for x in CPF2:
-    soma += int(x) * cont
-    cont -= 1
-soma = 11 - (soma % 11)
-CPF2 += str(0) if soma > 9 else str(soma)
+if CPF2[1]*9 == CPF2:
+    print('Numeros CPF sequenciais')
+else:
+    for x in CPF2:
+        soma += int(x) * cont
+        cont -= 1
+    soma = 11 - (soma % 11)
+    CPF2 += str(0) if soma > 9 else str(soma)
 
-cont = 11
-soma = 0
-for x in CPF2:
-    soma += int(x) * cont
-    cont -= 1
-soma = 11 - (soma % 11)
-CPF2 += str(0) if soma > 9 else str(soma)
-print('CPF Válido' if CPF2 == CPFteste else 'CPF Inválido')
+    cont = 11
+    soma = 0
+    for x in CPF2:
+        soma += int(x) * cont
+        cont -= 1
+    soma = 11 - (soma % 11)
+    CPF2 += str(0) if soma > 9 else str(soma)
+    print('CPF Válido' if CPF2 == CPF else 'CPF Inválido')
